1ef4e72cb228255c72669788e45a895671470043
Yang Hu <nyuhuyang@gmail.com>
2020-01-27 18:21:41 -0500

    filter, re-run ica and check by elbowplot

--- R/Seurat3/Lung_28/Rscripts/FurtherSubset_Reclustering.R
+++ R/Seurat3/Lung_28/Rscripts/FurtherSubset_Reclustering.R
@@ -24,8 +24,6 @@ print(paste0("slurm_arrayid=",args))
 
 groups <- c("AT","B","D","En","F","Mon","SAE","SMG","SMP","T")
 (g <- groups[args])
-save.path <- paste0(path,g,"/")
-if(!dir.exists(save.path))dir.create(save.path, recursive = T)
 
 # ========== cell types and markers ==============
 # En (CDH5) 
@@ -37,8 +35,12 @@ if(!dir.exists(save.path))dir.create(save.path, recursive = T)
 # AT (SFTPC,SCGB1A1+)
 # B, T, Mon (SRGN, LAPTM5)
 # SAE:Sec (SCGB1A1-very high)
-step = 1
+step = 4
 if(step == 1){
+        # create folder
+        save.path <- paste0(path,g,"/before-filter/")
+        if(!dir.exists(save.path))dir.create(save.path, recursive = T)
+        # markers
         markers <- list(c("En","CDH5"),
                 c("En","SRGN"),
                 c("SAE:C","CAPS"),
@@ -189,4 +191,118 @@ if(step == 1){
                       do.print = T, unique.name = "groups",  
                       save.path = save.path)
         save(object, file = paste0("data/Lung_28_",g,"_20200126.Rda"))
+}
+if(step == 2){
+        # create folder
+        save.path <- paste0(path,g,"/re-run-umap/")
+        if(!dir.exists(save.path))dir.create(save.path, recursive = T)
+        # load data
+        load(file = paste0("data/Lung_28_",g,"_20200126.Rda"))
+        DefaultAssay(object) = "SCT"
+        #======1.6 intergration =========================
+        DefaultAssay(object) = "SCT"
+        Seurat_list <- SplitObject(object, split.by = "conditions")
+        (size <- sapply(Seurat_list, ncol))
+        Seurat_list[size < 20] = NULL
+        Seurat_list %<>% lapply(SCTransform)
+        object.features <- SelectIntegrationFeatures(Seurat_list, nfeatures = 3000)
+        options(future.globals.maxSize= object.size(Seurat_list)*3)
+        Seurat_list %<>% PrepSCTIntegration(anchor.features = object.features, verbose = FALSE)
+        size <- sapply(Seurat_list, ncol)
+        (k.filter <- min(size - 1, 200))
+        (dims <- min(size - 2, 50))
+        anchors <- FindIntegrationAnchors(Seurat_list, normalization.method = "SCT", 
+                                          k.filter = k.filter,
+                                          dims = 1:dims,
+                                          anchor.features = object.features)
+        remove(Seurat_list);GC()
+        object <- IntegrateData(anchorset = anchors, normalization.method = "SCT", k.weight=30)
+        remove(anchors);GC()
+        object <- RunICA(object, verbose =F,nics = 50)
+        npcs = 50
+        object %<>% FindNeighbors(reduction = "ica",dims = 1:npcs)
+        object %<>% FindClusters(resolution = 0.1)
+        object %<>% RunTSNE(reduction = "ica", dims = 1:npcs, check_duplicates = FALSE)
+        object %<>% RunUMAP(reduction = "ica", dims = 1:npcs)
+        # cluster
+        UMAPPlot.1(object, group.by = "integrated_snn_res.0.1",
+                   label = T,
+                   label.repel = T, pt.size = 0.5,label.size = 3, repel = T,no.legend = T,
+                   do.print = T,do.return = F, unique.name = "groups",
+                   save.path = save.path,
+                   title = paste("Clusters in", g))
+        # cell types
+        Idents(object) = "cell_types"
+        object %<>% sortIdent()
+        object <- AddMetaColor(object = object, label= "cell_types", colors = c(Singler.colors,Singler.colors))
+        
+        UMAPPlot.1(object, group.by = "cell_types",cols = ExtractMetaColor(object),label = T,
+                   label.repel = T, pt.size = 1,label.size = 4, repel = T,no.legend = T,
+                   do.print = T,do.return = F,
+                   unique.name = "groups", save.path = save.path,
+                   title = paste("Cell types in", g))
+        
+        save(object, file = paste0("data/Lung_28_",g,"_20200126.Rda"))
+}
+
+# serial resolution and ICA heatmap on integrated data
+if(step == 3){
+        res_path <- paste0(path,g,"/serial_resolution/")
+        if(!dir.exists(res_path))dir.create(res_path, recursive = T)
+        ica_path <- paste0(path,g,"/ica_heatmaps/")
+        if(!dir.exists(ica_path))dir.create(ica_path, recursive = T)
+        load(file = paste0("data/Lung_28_",g,"_20200126.Rda"))
+        DefaultAssay(object) <- 'integrated'
+        
+        res = c(seq(0.01,0.1, by = 0.01),seq(0.2,1.6, by = 0.1))
+        for(i in seq_along(res)){
+                object %<>% FindClusters(resolution = res[i])
+                Idents(object) = paste0("integrated_snn_res.",res[i])
+                UMAPPlot.1(object, group.by=paste0("integrated_snn_res.",res[i]),pt.size = 0.3,label = T,
+                           label.repel = T,alpha = 0.9,
+                           do.return = F,
+                           no.legend = T,label.size = 4, repel = T, 
+                           title = paste("res =",res[i],"in",g," based on ICA"),
+                           unique.name = "groups",
+                           do.print = F, save.path = res_path)
+                ElbowPlot.2(object, graph.name = "integrated_snn",)
+                Progress(i,length(res))
+        }
+        
+        object <- RunICA(object, verbose =F,nics = 100)
+        a <- seq(1,50, by = 6)
+        b <- a+5
+        for(i in seq_along(a)){
+                jpeg(paste0(ica_path,"DimHeatmap_ica_",
+                            g,"_",a[i],"_",min(b[i],50),".jpeg"),
+                     units="in", width=10, height=7,res=600)
+                DimHeatmap(object, dims = a[i]:min(b[i],50),
+                           nfeatures = 30,reduction = "ica")
+                dev.off() 
+        }
+}
+
+# generate ElbowPlot
+if(step == 4){
+        ElbowPlot_path <- paste0(path,g,"/ElbowPlot/")
+        if(!dir.exists(ElbowPlot_path))dir.create(ElbowPlot_path, recursive = T)
+        load(file = paste0("data/Lung_28_",g,"_20200126.Rda"))
+        DefaultAssay(object) <- 'integrated'
+        object <- RunICA(object, verbose =F,nics = 100)
+        ElbowPlot.1(object, object, ndims = 100, reduction = "ica",
+                    title = paste("Standard Deviation against ICA numbers in cell type",g),
+                    do.print = T,do.return = F, save.path = ElbowPlot_path)
+        res = c(seq(0.01,0.1, by = 0.01),seq(0.2,1.6, by = 0.1))
+        for(i in seq_along(res)){
+                object %<>% FindClusters(resolution = res[i])
+                Progress(i,length(res))
+        }
+        ElbowPlot.2(object, graph.name = "integrated_snn",check.by = "Cluster.numbers",
+                    unique.name = "groups",
+                    title = paste("Total distance against different cluster numbers in cell type",g),
+                    do.print = T,do.return = F, save.path = ElbowPlot_path)
+        ElbowPlot.2(object, graph.name = "integrated_snn",check.by = "Resolutions",
+                    unique.name = "groups",
+                    title = paste("Total distance against different resolutions in cell type",g),
+                    do.print = T,do.return = F, save.path = ElbowPlot_path)
 }
