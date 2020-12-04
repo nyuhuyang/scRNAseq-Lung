ae8d1e38d45fb1d39a849394c3a34a8373f44dad
Yang Hu <hu.yang@nyu.edu>
2020-12-03 22:27:14 -0500

    DotPlot

--- R/Seurat3/Lung_30/Figures.R
+++ R/Seurat3/Lung_30/Figures.R
@@ -98,12 +98,12 @@ DotPlot(s_d, features = c("SCGB1A1","SCGB3A1","SCGB3A2","SFTPB","MUC5B"),
     coord_flip() + RotatedAxis()
 
 # Color of dots (blue, green, yellow, red; see example of color range in Figure 5a in the attached paper) - expression level.
+path = "Yang/Lung_30/Dotplots/No Clustering/"
 Idents(object) = "annotations3"
-object %<>% RenameIdents('S-d' = 'A', '2' = 'C')
 
 groups = c("Epithelial","Structural","Immune")
 for(i in 1:length(groups)){
-    df <- readxl::read_excel("doc/30 DEGs for dot plots.xlsx",
+    df <- readxl::read_excel("doc/30 DEGs for dot plots - revised 12-1-20.xlsx",
                              sheet = groups[i])
     cell.types = colnames(df)[2:ncol(df)]
     genes = df$...1
@@ -112,19 +112,38 @@ for(i in 1:length(groups)){
     sub_object@meta.data$annotations3 %<>% factor(levels = cell.types)
     Idents(sub_object) = "annotations3"
     
-    g <- DotPlot.1(sub_object, features = rev(genes),
-                   log.data = log2,
-                   scale = TRUE,cluster.idents = FALSE,
-            cols = c("blue","cyan","green","yellow","orange","red")
-            )+
-        ggtitle(groups[i])+ TitleCenter()+
-        theme(panel.border = element_rect(colour = "black", fill=NA, size=1),
-              plot.title = element_text(face = "plain"))+
-        coord_flip() + RotatedAxis()
-    jpeg(paste0(path,"Dotplot_",groups[i],".jpeg"), units="in", width=9, height=10,res=600)
-    print(g)
-    dev.off()
-    
+    options <- c("Max_6","Max_7","Max_auto")[2]
+    for(k in 1:length(options)){
+        save.path <- paste0(path,options[k],"/")
+        if(!dir.exists(save.path))dir.create(save.path, recursive = T)
+        exp.max = switch(options[k],
+                         "Max_6" = 6,
+                         "Max_7" = 7,
+                         "Max_auto" = NA)
+        g <- DotPlot.1(sub_object, features = rev(genes),
+                       log.data = log2,exp.max = exp.max,
+                       scale = TRUE,
+                       cluster.idents = FALSE,
+                       cluster.features = FALSE,
+                       cols = c("blue","green","yellow","orange","chocolate1","red"))+
+            ggtitle(groups[i])+ TitleCenter()+
+            theme(panel.border = element_rect(colour = "black", fill=NA, size=1),
+                  text = element_text(size=16),
+                  axis.text.x = element_text(size=16,
+                                             angle = 90, 
+                                             hjust = 1),
+                  axis.text.y = element_text(size=16),
+                  
+                  plot.title = element_text(face = "plain"))+
+            coord_flip()
+        width = switch(groups[i],
+                       "Epithelial" = 9,
+                       "Structural" = 7.5,
+                       "Immune" = 8.5)
+        jpeg(paste0(save.path,"Dotplot_",groups[i],".jpeg"), family = "Arial",
+             units="in", width=width, height=10,res=900)
+        print(g)
+        dev.off()
+    }
 }
-
 
